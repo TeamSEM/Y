b30e5dc1bcaca4d386b4c798cb6f3bbaf588736b
Molik Kidd <mzkidd5@gmail.com>
2023-02-16 12:25:33 -0500

    initial hash and login routes

--- tech-library-sem/server/routes/Users.js
+++ tech-library-sem/server/routes/Users.js
@@ -10,6 +10,74 @@ const {sequelize} = require("sequelize")
 router.use(express.json());
 router.use(express.urlencoded({extended: true}))
 
+const SALT_COUNT = 10;
+const {JWT_SECRET} = process.env;
+
+const setUser = async (req, res, next) => {
+    /* handler logic*/
+      try{
+      const auth = req.header('Authorization');
+          if(!auth) {
+              next();
+          } else {
+          const [,token] = auth.split(' ');
+          const user = jwt.verify(token, JWT_SECRET);
+          req.user = user;
+          next()
+          }
+      } catch({message}) {
+          res.sendStatus(401);
+          next({message})
+      }
+    }
+
+// POST /register
+// OPTIONAL - takes req.body of {username, password} and creates a new user with the hashed password
+// POST /register
+router.post('/register', setUser, async (req, res, next) => {
+
+    try {
+        const { username, password} = req.body;
+        // create a new user
+    
+        const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+        const {id, user} = await Users.create({
+        username: username,
+        password: hashedPassword
+        });
+        const token = jwt.sign({id,username}, process.env.JWT_SECRET);
+        res.send({message: 'success', token });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+  });
+
+  // POST /login
+// OPTIONAL - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+router.post('/login', setUser, async (req, res, next) => {
+
+    try {
+        const { username, password } = req.body;
+        const [foundUser] = await Users.findAll({
+            where: {username}
+        });
+         if(!foundUser) {
+            return 'Failed';
+        }
+        const comparePassword = await bcrypt.compare(password, foundUser.password);
+        if(comparePassword) {
+            const token = jwt.sign({id:foundUser.id,username:foundUser.username}, process.env.JWT_SECRET)
+            res.send({message: 'success', token});
+        } else {
+            return res.sendStatus(401);
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+  
+  })
 // read entries from the database
 router.get('/', async (req,res,next) => {
     try {
