5923d322643cbc1809e59cde9ab61c082aead05d
Salem Boyland <salemb@google.com>
2023-02-16 13:05:24 -0600

    tier 4

--- tech-library-sem/server/routes/Users.js
+++ tech-library-sem/server/routes/Users.js
@@ -43,4 +43,34 @@ router.post("/", async (req,res) => {
     res.json(await Users.findAll());
     })
 
+    // Keeing track of authorized users and their data
+
+    const authorizedUsers = [
+    { username: 'user1', password: 'password1', level: 1 },
+    { username: 'user2', password: 'password2', level: 2 },
+    
+    // Populate with / add more authorized users as needed
+    ];
+    
+    // Middleware is here checking if a user is authorized to access the API
+    const authorizeUser = (req, res, next) => {
+    const { username, password } = req.headers;
+    
+    // Checking to make sure the user is authorized
+
+    const authorizedUser = authorizedUsers.find(user => user.username === username && user.password === password);
+    
+    if (!authorizedUser) {
+    
+    // If the user is not authorized, send a 401 error with message
+
+    res.status(401).json({ error: 'You are not authorized to access this API.' });
+    } else {
+    
+    // If the user is authorized, store their authorization level in the request object and proceed to the next middleware
+    
+    req.userLevel = authorizedUser.level;
+    next();
+    }
+    };
 module.exports = router;
