be44e0644297e6f6e37e66d6f1e62d8cac2c6c58
Molik Kidd <mzkidd5@gmail.com>
2023-02-16 15:02:38 -0500

    Merge branch 'SalemBranch' of https://github.com/TeamSEM/Y into MZKBranch

--- tech-library-sem/server/seedData.js
+++
@@ -1,3 +0,0 @@
-// seedData for technoPhilosophers
-const techno_Philosophers = ["David Chalmers", "John Dewey", "Martin Heidegger",
-"Herbert Marcuse", "Ray Kurzweil", "Kevin Kelley", "Nick Bostrom" ];
