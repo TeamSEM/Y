5400c7f6d1138eb76787cc1cc0eb2db0e16609ae
Molik Kidd <mzkidd5@gmail.com>
2023-02-16 12:18:12 -0500

    add mock user form and install bcrypt and jwt

--- tech-library-sem/server/routes/Users.js
+++ tech-library-sem/server/routes/Users.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const {Users} = require("../models");
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const cors = require('cors');
 
+const bcrypt = require('bcrypt');
+const {sequelize} = require("sequelize")
 //include middleware for parsing the body included into the request
 router.use(express.json());
 router.use(express.urlencoded({extended: true}))
 
 // read entries from the database
-router.get('/', async (req,res) => {
-    await Users.get({
-        where:{
-            id: req.params.id
-        }
-    });
-    res.json(await Users.findAll());
-    })
+router.get('/', async (req,res,next) => {
+    try {
+        await Users.get({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.json(await Users.findAll());
+       
+    } catch (error) {
+        console.log(error);
+      next(error); 
+    }
+})
 
     //edit entries in the database
 router.put('/:id', async (req, res) => {
@@ -41,7 +50,7 @@ router.delete('/:userid', async (req,res)=>{
 router.post("/", async (req,res) => {
 
     console.log('added item', req.body )
-    const itemtoAdd = await Items.create(req.body);
+    // const itemtoAdd = await Items.create(req.body);
     res.json(await Users.findAll());
     })
 
